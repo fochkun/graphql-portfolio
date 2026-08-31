fb53435634d0be827885cee2247564c00ac27599
fochkun <dvchervakov@gmail.com>
2026-08-31 19:15:11 +0300

    add configModule

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  port: parseInt(process.env.PORT || '3000', 10) || 3000,
+  database: {
+    url: process.env.DATABASE_URL,
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10) || 5432,
+  },
+});
